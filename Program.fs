b88080d9892667f3f2ef8c4b4d0dd1d49b4a73df
Adnan Ioricce <adnanioricce@outlook.com>
2021-05-21 22:57:01 -0300

    finishing implementation. adapt to receive image file path as parameter would be good

--- Program.fs
+++ Program.fs
@@ -3,7 +3,8 @@
 open System.IO
 open SixLabors.ImageSharp
 open SixLabors.ImageSharp.PixelFormats
-
+open SixLabors.ImageSharp.PixelFormats;
+open SixLabors.ImageSharp.Processing;
 type Pixel = {
     R:byte
     G:byte
@@ -32,23 +33,24 @@ let pixelArray (image:Image<Rgba32>) = [|
 |]
 let max arr = arr |> Array.max
 let min arr = arr |> Array.min
-let averagePixel pixel = (pixel.R + pixel.G + pixel.B) / (byte 3)
-let lightness pixel = (max [|pixel.R;pixel.G;pixel.B|]) + (min [|pixel.R;pixel.G;pixel.B|]) / (byte 2)
+let averagePixel pixel = float ((pixel.R + pixel.G + pixel.B) / (byte 3))
+let lightness pixel = float ((max [|pixel.R;pixel.G;pixel.B|]) + (min [|pixel.R;pixel.G;pixel.B|]) / (byte 2))
 let luminosity pixel = (0.21 * (float pixel.R)) + (0.72 * (float pixel.G)) + (0.07 * (float pixel.B))
 let transformMatrix transformation matrix = matrix |> Array.map transformation
 let alphabet = """`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"""
 let pixelToAscii pixel = int (((pixel / 255.0) * 100.0) % (float alphabet.Length))
 let asciiImage arr = arr |> Array.map (fun pixel -> alphabet |> Seq.item (pixelToAscii pixel))
-    
+let resize (image:Image<Rgba32>) = 
+    image.Clone(fun ctx -> ctx.Resize(image.Width / 2,image.Height / 2).Grayscale() |> ignore)
 [<EntryPoint>]
 let main argv =    
-    let image = loadImage "imagem_teste.png"    
-    let arr = pixelArray image
-    let averagedPixels = arr |> Array.map luminosity  
-    let asciiPixelsIndex = averagedPixels |> Array.map pixelToAscii
-    // printfn "%A" asciiPixelsIndex
-    let ascii = (asciiImage (arr |> Array.map luminosity)) |> Array.map string |> String.concat ""
-    printfn "%s" ascii
-    // for chr in ascii do
-    //     printfn "%c" chr
+    let image = loadImage "imagem_teste.png"
+    let resizedImage = resize image
+    let arr = pixelArray resizedImage
+    let averagedPixels = arr |> Array.map luminosity
+    let asciiPixelsIndex = averagedPixels |> Array.map pixelToAscii    
+    for row in asciiPixelsIndex |> Array.chunkBySize image.Width do
+        let chars = row |> Array.map (fun p -> string alphabet.[p]) |> String.concat ""
+        printfn "%s" chars
+    
     0 // return an integer exit code
